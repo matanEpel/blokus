32ae82fa55533fcd5ddc0d8ad105fe17b3a5ad1a
matanepel <matan.epel12@gmail.com>
2022-03-27 14:30:01 +0300

    new min_dists

--- blokus_problems.py
+++ blokus_problems.py
@@ -100,13 +100,14 @@ class BlokusCornersProblem(SearchProblem):
 
         return sum
 
+
 def new_min_dists(state, locations, max_int):
     points = np.asarray(np.where(np.array(state.state) != -1))
     dists = [max_int] * len(locations)
     for i in range(len(locations)):
-        dist_2 = np.sum(np.array([(points[0] - locations[i][0]) ** 2,(points[1] - locations[i][1]) ** 2]), axis=0)
-        closest = points[:,np.argmin(dist_2)]
-        dists[i] = math.sqrt((closest[0] - locations[i][0])**2 + (closest[1] - locations[i][1])**2)
+        dist_2 = np.sum(np.array([(points[0] - locations[i][0]) ** 2, (points[1] - locations[i][1]) ** 2]), axis=0)
+        closest = points[:, np.argmin(dist_2)]
+        dists[i] = math.sqrt((closest[0] - locations[i][0]) ** 2 + (closest[1] - locations[i][1]) ** 2)
     return dists
 
 
