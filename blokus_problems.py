c33448b0561d841be1cb15f240187fe491285c15
matanepel <matan.epel12@gmail.com>
2022-03-27 18:55:11 +0300

    closest location

--- blokus_problems.py
+++ blokus_problems.py
@@ -194,13 +194,18 @@ def blokus_cover_heuristic(state, problem):
     return a
 
 
+def distance(t1, t2):
+    return math.sqrt((t1[0] - t2[0]) ** 2 + (t1[1] - t2[1]) ** 2)
+
+
 class ClosestLocationSearch:
     """
     In this problem you have to cover all given positions on the board,
     but the objective is speed, not optimality.
     """
 
-    def __init__(self, board_w, board_h, piece_list, starting_point=(0, 0), targets=(0, 0)):
+    def __init__(self, board_w, board_h, piece_list, starting_point=(0, 0), targets=[(0, 0)]):
+        self.board = Board(board_w, board_h, 1, piece_list, starting_point)
         self.expanded = 0
         self.targets = targets.copy()
         "*** YOUR CODE HERE ***"
@@ -211,27 +216,173 @@ class ClosestLocationSearch:
         """
         return self.board
 
+    def is_goal_state(self, state):
+        for tup in self.targets:
+            i, j = tup
+            if state.state[i, j] == -1:
+                return False
+
+        return True
+
+    def reached_a_target(self, state, targets):
+        t = []
+        for tup in targets:
+            i, j = tup
+            if state.state[i, j] != -1:
+                t.append(tup)
+        return t
+
+    def get_dist_from_targets(self, state, targets):
+        """mini = state.board_w * state.board_h
+        for targ in targets:
+            dist = get_dist_from_targets(state, targ)
+            if dist < mini:
+                mini = dist
+        return mini"""
+        max_int = state.board_w * state.board_h  # a number that can be used as max int to the distances in the board
+        locations = targets
+        dists = [max_int] * len(locations)
+
+        # if the cover is filled:
+        for i in range(len(locations)):
+            if state.state[locations[i][0], locations[i][1]] != -1:
+                dists[i] = 0
+
+        valid_places_list = []
+        for i in range(0, state.board_w):
+            for j in range(0, state.board_h):
+                if state.state[i, j] != -1:
+                    valid_places_list.append((j, i))
+
+        for place in valid_places_list:
+            for i in range(len(locations)):
+                if distance(place, locations[i]) < dists[i]:
+                    dists[i] = distance(place, locations[i])
+
+        mini = max_int
+        idx = -1
+        for i in range(len(locations)):
+            if dists[i] == 0:
+                continue
+            elif dists[i] < mini:
+                mini = dists[i]
+                idx = i
+
+        return mini, locations[idx]
+
+    def get_dist_from_target(self, state, target):
+        """mini = state.board_w * state.board_h
+        for targ in targets:
+            dist = get_dist_from_targets(state, targ)
+            if dist < mini:
+                mini = dist
+        return mini"""
+        max_int = state.board_w * state.board_h
+        dists = max_int
+
+        # if the cover is filled:
+        if state.state[target[0], target[1]] != -1:
+            return 0
+
+        valid_places_list = []
+        for i in range(0, state.board_w):
+            for j in range(0, state.board_h):
+                if state.state[i, j] != -1:
+                    valid_places_list.append((j, i))
+
+        for place in valid_places_list:
+            if distance(place, target) < dists:
+                dists = distance(place, target)
+        return dists
+
+    def get_successors(self, state, targets):
+        self.expanded = self.expanded + 1
+        succs = [(state.do_move(0, move), move, move.piece.get_num_tiles()) for move in state.get_legal_moves(0)]
+        d = {}
+        for succ in succs:
+            d[succ] = self.get_dist_from_targets(succ[0], targets)[0]
+        d = sorted(d, key=d.get)
+        return d
+
+    def get_successors_one_target(self, state, target):
+        self.expanded = self.expanded + 1
+        succs = [(state.do_move(0, move), move, move.piece.get_num_tiles()) for move in state.get_legal_moves(0)]
+        d = {}
+        for succ in succs:
+            d[succ] = self.get_dist_from_target(succ[0], target)
+        d = sorted(d, key=d.get)
+        return d
+
     def solve(self):
-        """
-        This method should return a sequence of actions that covers all target locations on the board.
-        This time we trade optimality for speed.
-        Therefore, your agent should try and cover one target location at a time. Each time, aiming for the closest uncovered location.
-        You may define helpful functions as you wish.
+        remaining_targets = self.targets.copy()
 
-        Probably a good way to start, would be something like this --
+        s = util.Stack()
+        d = {}
+        visited = set()
+        start = self.get_start_state()
+        s.push((start, []))
+        d[start] = None
+        while not s.isEmpty():
+            u, reached = s.pop()
+            # if not u in visited:
+            visited.add(u)
+            for tup in self.get_successors(u, remaining_targets):
+                if tup[0] in visited:
+                    continue
+                a = tup[0]
+                d[a] = (u, tup[1])
 
-        current_state = self.board.__copy__()
-        backtrace = []
+                reached = self.reached_a_target(a, remaining_targets)
+                s.push((a, reached))
 
-        while ....
+                if reached:
+                    temp_reached = [targ for targ in remaining_targets if targ not in reached]
+                    if not temp_reached:
+                        curr_state = a
+                        l = []
+                        while d[curr_state] != None:
+                            l.append(d[curr_state][1])
+                            curr_state = d[curr_state][0]
+                        return l[::-1]
 
-            actions = set of actions that covers the closets uncovered target location
-            add actions to backtrace
+    def solved(self):
+        remaining_targets = self.targets.copy()
 
-        return backtrace
-        """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        s = util.Stack()
+        d = {}
+        visited = set()
+
+        start = self.get_start_state()
+        s.push((start, []))
+        d[start] = None
+        _, target = self.get_dist_from_targets(start, remaining_targets)
+        while not s.isEmpty():
+            u, reached = s.pop()
+            if self.get_dist_from_target(u, target) == 0:
+                remaining_targets.remove(target)
+                _, target = self.get_dist_from_targets(start, remaining_targets)
+            print(remaining_targets)
+
+            # if not u in visited:
+            visited.add(u)
+            for tup in self.get_successors_one_target(u, target):
+                if tup[0] in visited:
+                    continue
+                a = tup[0]
+                d[a] = (u, tup[1])
+
+                reached = self.reached_a_target(a, remaining_targets)
+                s.push((a, reached))
+
+                if reached:
+                    temp_reached = [targ for targ in remaining_targets if targ not in reached]
+                    if not temp_reached:
+                        curr_state = a
+                        l = []
+                        while d[curr_state] != None:
+                            l.append(d[curr_state][1])
+                            curr_state = d[curr_state][0]
+                        return l[::-1]
 
 
 class MiniContestSearch:
