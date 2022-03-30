51a5bcf932bc62e78d3fa4e03cadc37fd1c6effa
matanepel <matan.epel12@gmail.com>
2022-03-30 17:52:57 +0300

    Closest locaion

--- blokus_problems.py
+++ blokus_problems.py
@@ -219,30 +219,36 @@ class ClosestLocationSearch:
     def __init__(self, board_w, board_h, piece_list, starting_point=(0, 0), targets=[(0, 0)]):
         self.board = Board(board_w, board_h, 1, piece_list, starting_point)
         self.expanded = 0
+        self.starting_point = starting_point
         self.targets = targets.copy()
+        self.curr_target = None
+        self.curr_state_finished_last = self.board
+        self.start = (0,0)
         "*** YOUR CODE HERE ***"
 
     def get_start_state(self):
         """
         Returns the start state for the search problem
         """
-        return self.board
+        return self.curr_state_finished_last
+
+    def get_cost_of_actions(self, actions):
+        """
+        the cost of all the actions in a list of actions
+        :param actions: the list of actions
+        :return: the cost
+        """
+        sum = 0
+        for move in actions:
+            sum += move.piece.get_num_tiles()
+        return sum
 
     def is_goal_state(self, state):
-        for tup in self.targets:
-            i, j = tup
-            if state.state[i, j] == -1:
-                return False
-
+        i, j = self.curr_target
+        if state.state[i, j] == -1:
+            return False
+        self.curr_state_finished_last = state
         return True
-
-    def reached_a_target(self, state, targets):
-        t = []
-        for tup in targets:
-            i, j = tup
-            if state.state[i, j] != -1:
-                t.append(tup)
-        return t
 
     def get_dist_from_targets(self, state, targets):
         """mini = state.board_w * state.board_h
@@ -266,6 +272,9 @@ class ClosestLocationSearch:
                 if state.state[i, j] != -1:
                     valid_places_list.append((j, i))
 
+        if not valid_places_list:
+            valid_places_list.append(self.starting_point)
+
         for place in valid_places_list:
             for i in range(len(locations)):
                 if distance(place, locations[i]) < dists[i]:
@@ -282,119 +291,28 @@ class ClosestLocationSearch:
 
         return mini, locations[idx]
 
-    def get_dist_from_target(self, state, target):
-        """mini = state.board_w * state.board_h
-        for targ in targets:
-            dist = get_dist_from_targets(state, targ)
-            if dist < mini:
-                mini = dist
-        return mini"""
-        max_int = state.board_w * state.board_h
-        dists = max_int
-
-        # if the cover is filled:
-        if state.state[target[0], target[1]] != -1:
-            return 0
-
-        valid_places_list = []
-        for i in range(0, state.board_w):
-            for j in range(0, state.board_h):
-                if state.state[i, j] != -1:
-                    valid_places_list.append((j, i))
-
-        for place in valid_places_list:
-            if distance(place, target) < dists:
-                dists = distance(place, target)
-        return dists
-
-    def get_successors(self, state, targets):
+    def get_successors(self, state):
+        """
+        we used the same function as yours
+        :param state: the curr state
+        :return: all the successors
+        """
         self.expanded = self.expanded + 1
-        succs = [(state.do_move(0, move), move, move.piece.get_num_tiles()) for move in state.get_legal_moves(0)]
-        d = {}
-        for succ in succs:
-            d[succ] = self.get_dist_from_targets(succ[0], targets)[0]
-        d = sorted(d, key=d.get)
-        return d
-
-    def get_successors_one_target(self, state, target):
-        self.expanded = self.expanded + 1
-        succs = [(state.do_move(0, move), move, move.piece.get_num_tiles()) for move in state.get_legal_moves(0)]
-        d = {}
-        for succ in succs:
-            d[succ] = self.get_dist_from_target(succ[0], target)
-        d = sorted(d, key=d.get)
-        return d
+        return [(state.do_move(0, move), move, move.piece.get_num_tiles()) for move in state.get_legal_moves(0)]
 
     def solve(self):
-        remaining_targets = self.targets.copy()
-
-        s = util.Stack()
-        d = {}
-        visited = set()
-        start = self.get_start_state()
-        s.push(start)
-        d[start] = None
-        while not s.isEmpty():
-            u = s.pop()
-            # if not u in visited:
-            visited.add(u)
-            for tup in self.get_successors(u, remaining_targets):
-                if tup[0] in visited:
-                    continue
-                a = tup[0]
-                d[a] = (u, tup[1])
-
-                reached = self.reached_a_target(a, remaining_targets)
-                s.push(a)
-
-                if reached:
-                    temp_reached = [targ for targ in remaining_targets if targ not in reached]
-                    if not temp_reached:
-                        curr_state = a
-                        l = []
-                        while d[curr_state] != None:
-                            l.append(d[curr_state][1])
-                            curr_state = d[curr_state][0]
-                        return l[::-1]
-
-    def solved(self):
-        remaining_targets = self.targets.copy()
-
-        s = util.Stack()
-        d = {}
-        visited = set()
-
-        start = self.get_start_state()
-        s.push((start, []))
-        d[start] = None
-        _, target = self.get_dist_from_targets(start, remaining_targets)
-        while not s.isEmpty():
-            u, reached = s.pop()
-            if self.get_dist_from_target(u, target) == 0:
-                remaining_targets.remove(target)
-                _, target = self.get_dist_from_targets(start, remaining_targets)
-            print(remaining_targets)
-
-            # if not u in visited:
-            visited.add(u)
-            for tup in self.get_successors_one_target(u, target):
-                if tup[0] in visited:
-                    continue
-                a = tup[0]
-                d[a] = (u, tup[1])
-
-                reached = self.reached_a_target(a, remaining_targets)
-                s.push((a, reached))
-
-                if reached:
-                    temp_reached = [targ for targ in remaining_targets if targ not in reached]
-                    if not temp_reached:
-                        curr_state = a
-                        l = []
-                        while d[curr_state] != None:
-                            l.append(d[curr_state][1])
-                            curr_state = d[curr_state][0]
-                        return l[::-1]
+        back = []
+        curr_state = self.get_start_state()
+        while self.targets:
+            print(self.start)
+            dist, targ = self.get_dist_from_targets(curr_state, self.targets)
+            self.curr_target = targ
+            moves = ucs(self)
+            curr_state = self.curr_state_finished_last
+            back += moves
+            self.targets.remove(targ)
+        self.curr_state_finished_last = self.board
+        return back
 
 
 class MiniContestSearch:
